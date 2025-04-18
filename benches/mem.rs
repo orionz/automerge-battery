65e300d0e02460f9f9485c158e5688c1ed6b72bf
Orion Henry <orion.henry@gmail.com>
2025-04-18 13:00:43 -0500

    use memory tracking from martins paper

--- benches/mem.rs
+++ benches/mem.rs
@@ -1,11 +1,8 @@
-use automerge::Automerge;
+use automerge::{Automerge};
 use automerge_battery::{big_paste_doc, poorly_simulated_typing_doc, text_splice_100};
 use divan::{black_box, Bencher};
-use std::time::Duration;
 
-use divan::AllocProfiler;
-#[global_allocator]
-static ALLOC: AllocProfiler = AllocProfiler::system();
+use trace_alloc::measure_memusage;
 
 const N: u64 = 100_000;
 
@@ -13,53 +10,54 @@ fn main() {
     divan::main();
 }
 
-#[inline(never)]
-#[divan::bench(max_time = Duration::from_secs(3))]
-fn drop_typing(bencher: Bencher) {
-    let data = poorly_simulated_typing_doc(N);
-    bencher.bench_local(|| {
-        let d = data.clone();
-        std::mem::drop(black_box(d))
-    })
-}
-
-#[inline(never)]
-#[divan::bench(max_time = Duration::from_secs(3))]
-fn drop_big_test(bencher: Bencher) {
-    let data = big_paste_doc(N);
-    bencher.bench_local(|| {
-        let d = data.clone();
-        std::mem::drop(black_box(d))
-    })
-}
-
-#[inline(never)]
-#[divan::bench(max_time = Duration::from_secs(3))]
-fn drop_text_splice_100(bencher: Bencher) {
-    let data = text_splice_100(N);
-    bencher.bench_local(|| {
-        let d = data.clone();
-        std::mem::drop(black_box(d))
-    })
-}
-
-#[inline(never)]
-#[divan::bench(max_time = Duration::from_secs(3))]
-fn load_text_splice_100(bencher: Bencher) {
-    let data = text_splice_100(N).save();
-    bencher.bench_local(|| Automerge::load(&data).unwrap())
-}
-
-#[inline(never)]
-#[divan::bench(max_time = Duration::from_secs(3))]
+#[divan::bench(sample_count = 1)]
 fn load_typing(bencher: Bencher) {
     let data = poorly_simulated_typing_doc(N).save();
-    bencher.bench_local(|| Automerge::load(&data).unwrap())
+    bencher.bench_local(|| {
+        let (peak, steady, result) = measure_memusage(|| {
+            Automerge::load(&data).unwrap()
+        });
+        black_box(result);
+        print_mem(peak,steady);
+    });
 }
 
-#[inline(never)]
-#[divan::bench(max_time = Duration::from_secs(3))]
-fn load_big_test(bencher: Bencher) {
+#[divan::bench(sample_count = 1)]
+fn load_big_paste(bencher: Bencher) {
     let data = big_paste_doc(N).save();
-    bencher.bench_local(|| Automerge::load(&data).unwrap())
+    bencher.bench_local(|| {
+        let (peak, steady, result) = measure_memusage(|| {
+            Automerge::load(&data).unwrap()
+        });
+        black_box(result);
+        print_mem(peak,steady);
+    });
 }
+
+#[divan::bench(sample_count = 1)]
+fn load_splice_100(bencher: Bencher) {
+    let data = text_splice_100(N).save();
+    bencher.bench_local(|| {
+        let (peak, steady, result) = measure_memusage(|| {
+            Automerge::load(&data).unwrap()
+        });
+        black_box(result);
+        print_mem(peak,steady);
+    });
+}
+
+fn print_value(label: &str, size: usize) {
+  if size > 1024 * 1024 * 10 {
+     let out = format!("{},{:0>3}", size / 1024 / 1000, size / 1024 % 1000);
+     println!(":: {}: {: >8} KB", label, out);
+  } else {
+     println!(":: {}: {: >8} KB", label, size / 1024);
+  }
+}
+
+fn print_mem(peak: usize, steady: usize) {
+        println!("");
+        print_value("peak", peak);
+        print_value("stdy", steady);
+}
+
